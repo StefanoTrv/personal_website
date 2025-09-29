d163fc708a6381b6f13d788e0d3bdb9646534a97
StefanoTrv <stefanotravasci@gmail.com>
2025-09-29 21:35:35 +0200

    Added one more photo Added a photo of mountains nears Forni di Sopra.

--- js/home.js
+++ js/home.js
@@ -349,7 +349,8 @@ window.onload = function () {
         ["{{ site.baseurl }}/assets/img/carousel/foto21.jpg", "https://en.wikipedia.org/wiki/Florence_Cathedral", "Florence Cathedral"],
         ["{{ site.baseurl }}/assets/img/carousel/foto22.jpg", "https://en.wikipedia.org/wiki/Florence_Cathedral", "Florence Cathedral"],
         ["{{ site.baseurl }}/assets/img/carousel/foto23.jpg", "https://en.wikipedia.org/wiki/Uffizi", "The Uffizi Gallery"],
-        ["{{ site.baseurl }}/assets/img/carousel/foto24.jpg", "https://en.wikipedia.org/wiki/Riva_del_Garda", "Riva del Garda"]
+        ["{{ site.baseurl }}/assets/img/carousel/foto24.jpg", "https://en.wikipedia.org/wiki/Riva_del_Garda", "Riva del Garda"],
+        ["{{ site.baseurl }}/assets/img/carousel/foto25.jpg", "https://it.wikipedia.org/wiki/Forni_di_Sopra", "Mountains near Forni di Sopra"]
     ];
     
     randomizePhoto(1, photo_array);
